a92a8d5754961bf35b08a5d28f37a479ad467ab9
seungyeon.baek <qorbaxk97@gmail.com>
2025-03-28 00:35:09 +0900

    chore : github actions 을 위한 셋팅

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    // GitHub Pages는 하위 경로에서 호스팅되므로 basePath와 assetPrefix를 설정해야 함
+    basePath: '/seungyeon',
+    assetPrefix: '/seungyeon/',
+  };
+  
+  module.exports = nextConfig;
