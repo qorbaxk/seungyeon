1e6b812b229a1bb8260a2a4d6f1521e1be7b6875
seungyeon.baek <qorbaxk97@gmail.com>
2025-03-28 01:10:10 +0900

    style : fade in scale animation 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      keyframes: {
+        fadeInScale: {
+          '0%': { opacity: '0', transform: 'scale(0.5)' },
+          '100%': { opacity: '1', transform: 'scale(1.2)' },
+        },
+      },
+      animation: {
+        fadeInScale: 'fadeInScale 1s ease-out forwards',
       },
     },
   },
