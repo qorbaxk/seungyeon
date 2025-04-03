ce8da7671fcb3c0ed41a6874abadfc7c6f67b1d8
seungyeon.baek <qorbaxk97@gmail.com>
2025-04-03 14:16:06 +0900

    fix : env 값에 대한 처리

--- next.config.js
+++ next.config.js
@@ -9,6 +9,11 @@ const nextConfig = {
     images: {
       unoptimized: true,
     },
+    env: {
+      NEXT_PUBLIC_YOUTUBE_API_URL: process.env.NEXT_PUBLIC_YOUTUBE_API_URL,
+      NEXT_PUBLIC_YOUTUBE_API_KEY: process.env.NEXT_PUBLIC_YOUTUBE_API_KEY,
+      NEXT_PUBLIC_YOUTUBE_PLAYLIST_ID: process.env.NEXT_PUBLIC_YOUTUBE_PLAYLIST_ID,
+    },
   }),
 };
 
